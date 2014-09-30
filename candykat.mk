eb5907447f2526ee7c81b29cf46a0051ce65fb03
rc420head <dennislebhardt@yahoo.com>
2014-09-30 06:45:20 -0500

    remove multicarrier

--- candykat.mk
+++ candykat.mk
@@ -6,8 +6,5 @@ $(call inherit-product, vendor/candykat/config/nfc_enhanced.mk)
 # Inherit some common Slim stuff.
 $(call inherit-product, vendor/candykat/config/common_full_phone.mk)
 
-# Inherit multicarrier.
-$(call inherit-product, vendor/candykat/config/common_multicarrier.mk)
-
 PRODUCT_NAME := candykat_d2lte
 PRODUCT_DEVICE := d2lte
